09f934b2204adc907708aeadc051a9f561c91f7f
Brian <brian_van_den_heuvel@me.com>
2016-03-22 13:53:23 +0100

    had to

---
+++ PayDay!/PayDay!-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PayDay!-Bridging-Header.h
+//  
+//
+//  Created by Brian van den Heuvel on 14/02/16.
+//
+//
+
+#ifndef PayDay__Bridging_Header_h
+#define PayDay__Bridging_Header_h
+
+#import <Google/Analytics.h>
+
+#endif /* PayDay__Bridging_Header_h */
